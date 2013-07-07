45c58d0d9eb1f2b3cf1f695b9e73769e39e4779b
stamceidas <stamceidas@gmail.com>
2013-07-07 14:57:10 +0300

    Updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  hello=fs.readFileSync(index.html);
+  hello=fs.readFileSync('index.html');
   response.send(hello.toString('utf-8'));
 });
 
